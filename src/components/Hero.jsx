f805fe7713694c531869a451424663923a092888
irhamkamil <irhamkamil8322@gmail.com>
2025-07-01 21:20:00 +0700

    memperbarui hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,10 @@ export default function Hero() {
   const navigate = useNavigate();
 
   return (
-    <div className=" relative w-full h-[500px] bg-cover bg-center bg-no-repeat mt-30 md:mt-0 bg-image">
+    <div
+      style={{ backgroundImage: "url('/hero-image.jpg')" }}
+      className=" relative w-full h-[500px] bg-cover bg-center bg-no-repeat mt-30 md:mt-0"
+    >
       <div className="absolute inset-0 bg-gradient-to-br from-gray-300/50 via-gray-800/40 to-gray-400/60"></div>
       <div className="relative max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-center h-full">
         <div className=" mb-10 md:mb-0 text-center">
